9ea6438f701535de70d65e4500325955f059d9e5
Rahul Soni <rahul.soni@aristocrat.com>
2022-07-01 16:31:49 +0530

    Time: 1 ms (81.23%), Space: 43.5 MB (60.83%) - LeetHub

---
+++ 82-remove-duplicates-from-sorted-list-ii/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,48 @@
+package algorithm2;
+
+import common.CustomLinkedList;
+import common.ListNode;
+
+public class RemoveDuplicatesFromSortedListII {
+	
+	static ListNode deleteDuplicates(ListNode head) {
+		
+		if (head == null) {
+			return head;
+		}
+
+		ListNode beforeHead = new ListNode(-1, head);
+		beforeHead.next = head;
+		ListNode prev = beforeHead;
+		ListNode cur = head;
+
+		while (cur != null) {
+			if (cur.next != null && cur.val == cur.next.val) {
+				// this loop is for continuous duplicate elements
+				while (cur.next != null && cur.val == cur.next.val) {
+					cur = cur.next;
+				}
+				// skip all the elements lies between prev and cur because they all are
+				// duplicate
+				prev.next = cur.next;
+			} else {
+				prev = prev.next;
+			}
+			cur = cur.next;
+		}
+		return beforeHead.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = CustomLinkedList.makeList(new int[] { 1, 1, 1, 2, 3 });
+
+		System.out.println("Linked list with duplicate");
+		CustomLinkedList.printList(head);
+
+		ListNode curHead = RemoveDuplicatesFromSortedListII.deleteDuplicates(head);
+
+		System.out.println("\nLinked list without duplicate");
+		CustomLinkedList.printList(curHead);
+	}
+
+}
